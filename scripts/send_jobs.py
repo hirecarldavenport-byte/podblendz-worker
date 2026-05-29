eb7c87e78425c2e9431155ae1741b21dd971805a
Carl Davenport <hirecarldavenport@gmail.com>
2026-05-29 12:29:21 -0400

    send_jobs.py

--- scripts/send_jobs.py
+++ scripts/send_jobs.py
@@ -13,7 +13,7 @@ RUNPOD_API_KEY = os.environ.get("RUNPOD_API_KEY")
 ENDPOINT_ID = os.environ.get("RUNPOD_ENDPOINT_ID")
 
 DELAY_BETWEEN_JOBS = 2
-MAX_EPISODES = 500
+MAX_EPISODES = 212
 
 S3_BUCKET = "podblendz-episode-audio"
 S3_PREFIX = "raw_audio/"
