237c72de4034c2765fbe86c9b54f928bff76af2d
Carl Davenport <hirecarldavenport@gmail.com>
2026-08-24 14:29:49 -0400

    archive scarper manual udpate

--- scripts/archive_scraper.py
+++ scripts/archive_scraper.py
@@ -1,8 +1,8 @@
 print("HELLO FROM NEW FILE")
 
 targets = [
-    ("lex_fridman", "https://lexfridman.com/podcast/"),
-    ("econtalk", "https://www.econtalk.org/"),
+    ("lex_fridman", "https://lexfridman.com/podcast"),
+    ("econtalk", "https://www.econtalk.org"),
     ("huberman_lab", "https://www.hubermanlab.com/podcast"),
 ]
 
