4350cf96bb173510be88ee47c63e132409f75c7b
Carl Davenport <hirecarldavenport@gmail.com>
2026-08-24 14:31:49 -0400

    updated acrive scraper

--- scripts/archive_scraper.py
+++ scripts/archive_scraper.py
@@ -1,9 +1,9 @@
 print("HELLO FROM NEW FILE")
 
 targets = [
-    ("lex_fridman", "https://lexfridman.com/podcast"),
-    ("econtalk", "https://www.econtalk.org"),
-    ("huberman_lab", "https://www.hubermanlab.com/podcast"),
+
+    ("a", "abc"),
+
 ]
 
 print(targets)
